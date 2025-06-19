ece574d8ff573db4668ac2a8d632296c48720d23
this-Aditya <adityamishra2217@gmail.com>
2025-06-19 21:59:38 +0530

    Created kotlin application

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    application
+    kotlin("jvm") version "1.9.10"
+}
+
+application {
+    mainClass.set("aditya.ga.kotlin.MainKt")
+}
---
+++ src/main/kotlin/aditya/ga/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package aditya.ga.kotlin
+
+fun main() {
+    println("Hi from the kotlin project. This is created to work out with github actions ")
+}
